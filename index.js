33468e04b6000171a7f748e84cfadf17f757048e
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-02-22 14:29:28 -0500

    adding in readme file

--- index.js
+++ index.js
@@ -1,87 +1,115 @@
 // TODO: Include packages needed for this application
 //imports the Inquier file and fs file
 const inquirer = require('inquirer');
+
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 //ask the user in nodes what question,and then put the information into a read me file
-function AskQuestion(){
 
-    inquirer
-        .prompt(
-            {
-                type: 'input',
-                message: 'What is the Title of your project?',
-                name: 'Title'
-            },
-            {
-                type: 'input',
-                message: 'What is the Description of your project?',
-                name: 'username'
-            },
-            {
-                type: 'input',
-                message: 'What is the installation instruction for this program?',
-                name: 'instruction',
-            },
-            {
-                type: 'input',
-                message: 'What is the Usage of your project?',
-                name: 'usage',
-            },
-            {
-                type: 'input',
-                message: 'What is the Contribution Guidelines of your project?',
-                name: 'contribution',
-            },
-            {
-                type: 'input',
-                message: 'What is the test instruction of your project?',
-                name: 'test',
-            },
-            {
-                type: 'list',
-                message: 'What License will your project use ?',
-                choice : ["Academic Free License v3.0","MIT","Mozilla Public License 2.0","Open Software License 3.0","The Unlicense"],
-                name: 'license',
-            },
-            {
-                type: 'input',
-                message: 'What is your git hub username?',
-                name: 'gitHub',
-            },
-            {
-                type: 'input',
-                message: 'What is your email address?',
-                name: 'email',
-            },
-        
-            {
-                type: 'input',
-                message: 'What Badge will your project use ?',
-                name: 'badge',
-            }
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the Title of your project?',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'What is the Description of your project?',
+            name: 'description'
+        },
+        {
+            type: 'input',
+            message: 'What is the installation instruction for this program?',
+            name: 'instruction',
+        },
+        {
+            type: 'input',
+            message: 'What is the Usage of your project?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Who Contributed to your project, Please provide name git hub user names?',
+            name: 'contribution',
+        },
+        {
+            type: 'input',
+            message: 'What is the test instruction of your project?',
+            name: 'test',
+        },
+        {
+            type: 'list',
+            message: 'What License will your project use ?',
+            choices: ["Academic Free License v3.0","MIT","Mozilla Public License 2.0","Open Software License 3.0","The Unlicense"],
+            name: 'license',
+        },
+        {
+            type: 'list',
+            message: 'What Badge color do you want ?',
+            choices: ["brightgreen","green","yellowgreen","yellow","orange","red","blue","lightgrey","blueviolet","purple","teal"],
+            name: 'badgeColor',
+        },
+        {
+            type: 'input',
+            message: 'What is your git hub username?',
+            name: 'gitHub',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
 
-        ).then((response) =>
+    ]);
 
-
-            console.log("my response are", response)
-      );
+    const generateReadMe = (answers)=>
+       `
+        # Title:
+        * ${answers.tite}
+        # Table Of Content:
+        * [Description](#Project Description:)
+        * [Installation](#Project Installation Instructions:)
+        * [Usage](#Project Usage:)
+        * [Contributing](#Project Contributors:)
+        * [Tests](#Project Test:)
+        * [License](#Project license:)
+        * [Contact Information](#Contact Information:)
+        #Project Description:
+        * ${answers.description}
+        #Project Installation Instructions: 
+        * ${answers.instruction}
+        #Project Usage:
+        * ${answers.usage}
+        #Project Contributors:
+        * ${answers.contribution}
+        #Project Test:
+        * ${answers.test}
+        #Project License:
+        * ${answers.license}
+        ![badge](https://img.shields.io/static/v1?label=Project_License&message=${answers.license}&color=${answers.badgeColor})
+        #Contact Information:
+        * If you have question or would like to contact the project creator please:
+            * Get Visit Git Hub profile at: (https://github.com/${gitHub})
+            * Email at: ${email}
+        `;
     
 
+
     
+    const init = () => {
+    promptUser().then((answers) => {
+        try {
+        const readme = generateReadMe(answers);
+        fs.writeFileSync('README.md', readme);
+        console.log('Successfully wrote to readme.html');
+        } catch (error) {
+        console.log(error);
+        }
+    });
+};
+  
 
 
-
-}
-
-AskQuestion();
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
+init();
