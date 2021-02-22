d97c855ece7ed8bbe25b06de7fcde401d33f8cce
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-02-22 15:02:18 -0500

    fix formatting

--- README.md
+++ README.md
@@ -1,38 +1,38 @@
 
-#   Title:
+# Title:
 *    undefined
 
-#    Table Of Content:
-*   [Description](#Project Description:)
-*   [Installation](#Project Installation Instructions:)
-*   [Usage](#Project Usage:)
-*   [Contributing](#Project Contributors:)
-*   [Tests](#Project Test:)
-*   [License](#Project license:)
+# Table Of Content:
+*   [Description](# Project Description:)
+*   [Installation](# Project Installation Instructions:)
+*   [Usage](# Project Usage:)
+*   [Contributing](# Project Contributors:)
+*   [Tests](# Project Test:)
+*   [License](# Project license:)
 *   [Contact Information](#Contact Information:)
 
-#   Project Description:
-*   With the answer of a series of question you will be able to create a read me file
+# Project Description:
+* With the answer of a series of question you will be able to create a read me file
 
-#   Project Installation Instructions: 
-*   Start the program with node index.js , that will start the program. Then you can answer the question to generate the readme file.
+# Project Installation Instructions: 
+* Start the program with node index.js , that will start the program. Then you can answer the question to generate the readme file.
 
-#   Project Usage:
-*   To create readMe files 
+# Project Usage:
+* To create readMe files 
 
-#   Project Contributors:
-*   No other contributors except the author.
+# Project Contributors:
+* No other contributors except the author.
 
-#   Project Test:
-*   No test at the moment for this project
+# Project Test:
+* No test at the moment for this project
 
-#   Project License:
-*   The Unlicense
-![badge](https://img.shields.io/static/v1?label=Project_License&message=The Unlicense&color=teal)
+# Project License:
+* The Unlicense
+* ![badge](https://img.shields.io/static/v1?label=Project_License&message=The_Unlicense&color=teal)
 
-#   Contact Information:
-*   If you have question or would like to contact the project creator please:
-    *   Get Visit Git Hub profile at: (https://github.com/Jacqueline8996)
-    *   Email at: Jacqueline8996@gmail.com
+# Contact Information:
+* If you have question or would like to contact the project creator please:
+    * Get Visit Git Hub profile at: (https://github.com/Jacqueline8996)
+    * Email at: Jacqueline8996@gmail.com
 
         
--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const promptUser = () =>
         {
             type: 'list',
             message: 'What License will your project use ?',
-            choices: ["Academic Free License v3.0","MIT","Mozilla Public License 2.0","Open Software License 3.0","The Unlicense"],
+            choices: ["Academic_Free_License_v3.0","MIT","Mozilla_Public_License_2.0","Open_Software_License_3.0","The_Unlicense"],
             name: 'license',
         },
         {
@@ -65,39 +65,39 @@ const promptUser = () =>
     //In order to get this to work yo have to move it as farleft as you can so the git hub recognize the formatting 
 
 ` 
-#  Title:
-*  ${answers.title}
+# Title:
+* ${answers.title}
 
-#  Table Of Content:
-*  [Description](#Project Description:)
-*  [Installation](#Project Installation Instructions:)
-*  [Usage](#Project Usage:)
-*  [Contributing](#Project Contributors:)
-*  [Tests](#Project Test:)
-*  [License](#Project license:)
-*  [Contact Information](#Contact Information:)
+# Table Of Content:
+* [Description](#Project Description:)
+* [Installation](#Project Installation Instructions:)
+* [Usage](#Project Usage:)
+* [Contributing](#Project Contributors:)
+* [Tests](#Project Test:)
+* [License](#Project license:)
+* [Contact Information](#Contact Information:)
 
-#  Project Description:
-*  ${answers.description}
+# Project Description:
+* ${answers.description}
 
-#  Project Installation Instructions: 
-*  ${answers.instruction}
+# Project Installation Instructions: 
+* ${answers.instruction}
 
-#  Project Usage:
-*  ${answers.usage}
+# Project Usage:
+* ${answers.usage}
 
-#  Project Contributors:
-*  ${answers.contribution}
+# Project Contributors:
+* ${answers.contribution}
 
-#  Project Test:
-*  ${answers.test}
+# Project Test:
+* ${answers.test}
 
-#  Project License:
-*  ${answers.license}
-![badge](https://img.shields.io/static/v1?label=Project_License&message=${answers.license}&color=${answers.badgeColor})
+# Project License:
+* ${answers.license}
+* ![badge](https://img.shields.io/static/v1?label=Project_License&message=${answers.license}&color=${answers.badgeColor})
 
-#  Contact Information:
-*  If you have question or would like to contact the project creator please:
+# Contact Information:
+* If you have question or would like to contact the project creator please:
     *  Get Visit Git Hub profile at: (https://github.com/${answers.gitHub})
     *  Email at: ${answers.email}`;
     
